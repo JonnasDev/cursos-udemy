99ef5031e9a444617fa2c506c071055e0e64da7e
Jotta_Dev <110307772+JonnasDev@users.noreply.github.com>
2023-06-14 14:55:48 -0300

    Add files via upload

---
+++ kotlin-basico/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+   var nome = "jonnas"
+    nome = "gabriel" //mutavel
+
+    //val nome = "jonnas" //imutavel
+    //const apenas para teste, ele é imutavel
+    println(nome)
+}
